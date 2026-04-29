4f0e0c581583f902f519fa0e9259ec886348db32
Jacob Smolik <jacobsmolik@Jacobs-MacBook-Air.local>
2026-04-29 18:23:25 -0500

    Bump service worker cache to v5 to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'route-optimizer-v1';
+const CACHE = 'route-optimizer-v5';
 const ASSETS = [
   'https://jacobsmolik1.github.io/RouteOptimizer/',
   'https://jacobsmolik1.github.io/RouteOptimizer/index.html',
