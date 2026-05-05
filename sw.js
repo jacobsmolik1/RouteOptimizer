3a697faf0207f000172cc590322082299fcd931d
Jacob Smolik <jacobsmolik@Jacobs-MacBook-Air.local>
2026-05-05 13:52:11 -0400

    Deprioritize domicile drivers for non-home loads, prioritize for home loads

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'route-optimizer-v11';
+const CACHE = 'route-optimizer-v12';
 const ASSETS = [
   'https://jacobsmolik1.github.io/RouteOptimizer/',
   'https://jacobsmolik1.github.io/RouteOptimizer/index.html',
