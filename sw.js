7cfa9338c35f140c6ace2eb6b33cfac6e6f8bdb3
Jacob Smolik <jacobsmolik@Jacobs-MacBook-Air.local>
2026-05-05 10:08:03 -0400

    Fix deadhead: morning-only init, evening DH settled in post-process

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'route-optimizer-v9';
+const CACHE = 'route-optimizer-v10';
 const ASSETS = [
   'https://jacobsmolik1.github.io/RouteOptimizer/',
   'https://jacobsmolik1.github.io/RouteOptimizer/index.html',
