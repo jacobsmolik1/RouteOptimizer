cba384945834f1da27573ded227637449ec602ac
Jacob Smolik <jacobsmolik@Jacobs-MacBook-Air.local>
2026-05-06 12:39:12 -0400

    Fix SW update banner: detect waiting SW on load, poll hourly for updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'route-optimizer-v39';
+const CACHE = 'route-optimizer-v40';
 const ASSETS = [
   'https://jacobsmolik1.github.io/RouteOptimizer/',
   'https://jacobsmolik1.github.io/RouteOptimizer/index.html',
